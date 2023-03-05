1b2f2cebeafea4ebde4fe08d2352d5f3b3aee020
naji-alhusami <najihussami@gmail.com>
2023-03-05 10:58:08 +0300

    added store react redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import { store } from "./store/store";
+import { Provider } from 'react-redux';
+import { store } from "./store/store";
 import App from './App';
 import './index.css';
 
@@ -12,9 +12,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <Provider store={store}> */}
+      <Provider store={store}>
       <App style={{ fontFamily: 'Poppins, sans-serif' }} />
-      {/* </Provider> */}
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
