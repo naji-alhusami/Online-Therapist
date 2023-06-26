604afe8fb47eb5cdc9f16c425db16a7a65df1627
naji-alhusami <najihussami@gmail.com>
2023-06-26 23:55:17 +0300

    comment out the cardsSlice until I finish the store

--- src/features/cards/cardsSlice.js
+++ src/features/cards/cardsSlice.js
@@ -1,39 +1,39 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+// import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-// start of Add Credit Card:
-export const addCreditCard = createAsyncThunk(
-  'card/addCreditCard',
-  async (payload) => {
-    // const { name, number, expiration, cvc } = payload;
-    // console.log(payload);
-  }
-);
-// End of Add Credit Card.
+// // start of Add Credit Card:
+// export const addCreditCard = createAsyncThunk(
+//   'card/addCreditCard',
+//   async (payload) => {
+//     // const { name, number, expiration, cvc } = payload;
+//     // console.log(payload);
+//   }
+// );
+// // End of Add Credit Card.
 
-const cardsSlice = createSlice({
-  name: 'users',
-  initialState: {
-    loading: false,
-    card: {},
-    error: null,
-  },
+// const cardsSlice = createSlice({
+//   name: 'users',
+//   initialState: {
+//     loading: false,
+//     card: {},
+//     error: null,
+//   },
 
-  extraReducers: (builder) => {
-    // Add Credit Card Cases:
-    builder.addCase(addCreditCard.pending, (state) => {
-      state.loading = true;
-    });
-    builder.addCase(addCreditCard.fulfilled, (state, action) => {
-      // state.loading = false;
-      state.card = action.payload;
-      state.signedup = true;
-    });
-    builder.addCase(addCreditCard.rejected, (state, action) => {
-      state.loading = false;
-      state.card = {};
-      state.error = action.payload;
-    });
-  },
-});
+//   extraReducers: (builder) => {
+//     // Add Credit Card Cases:
+//     builder.addCase(addCreditCard.pending, (state) => {
+//       state.loading = true;
+//     });
+//     builder.addCase(addCreditCard.fulfilled, (state, action) => {
+//       // state.loading = false;
+//       state.card = action.payload;
+//       state.signedup = true;
+//     });
+//     builder.addCase(addCreditCard.rejected, (state, action) => {
+//       state.loading = false;
+//       state.card = {};
+//       state.error = action.payload;
+//     });
+//   },
+// });
 
-export default cardsSlice.reducer;
+// export default cardsSlice.reducer;
